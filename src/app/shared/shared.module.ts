5e2e7b28e4b49615b3ed067250a2d31fca253abb
jeison003 <jeisonmartinezev@unimagdalena.edu.co>
2023-07-05 18:14:59 -0500

    version final gifsApp

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    LazyImageComponent
+    LazyImageComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     SidebarComponent,
-    LazyImageComponent
+    LazyImageComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
